05f264a23646530ad5d02ca47d2016f9b7c709c8
xemyn <105799880+xemyn@users.noreply.github.com>
2022-11-03 13:17:52 +0100

    Add files via upload
    werkgroep 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 console.log("Howdy!");
+
+var element1 = document.querySelector("section:nth-of-type(6)");
+element1.addEventListener("click", toggleDoor);
+
+// function toggleDoor() {
+// element1.classList.toggle("rechterdeurOpen");
+// }
+
+
+var element2 = document.querySelector("section:nth-of-type(3)");
+element2.addEventListener("click", toggleDoor);
+
+function toggleDoor() {
+element2.classList.toggle("linkerdeurOpen");
+element1.classList.toggle("rechterdeurOpen");
+}
