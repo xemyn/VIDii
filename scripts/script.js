3e5d8b8417cca010b24a70cb0ddaa624861e087a
xemyn <105799880+xemyn@users.noreply.github.com>
2022-11-06 11:53:15 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,20 @@ console.log("Howdy!");
 var element1 = document.querySelector("section:nth-of-type(6)");
 element1.addEventListener("click", toggleDoor);
 
-// function toggleDoor() {
-// element1.classList.toggle("rechterdeurOpen");
-// }
-
-
 var element2 = document.querySelector("section:nth-of-type(3)");
 element2.addEventListener("click", toggleDoor);
 
 function toggleDoor() {
-element2.classList.toggle("linkerdeurOpen");
-element1.classList.toggle("rechterdeurOpen");
+    element2.classList.toggle("linkerdeurOpen");
+    element1.classList.toggle("rechterdeurOpen");
+}
+
+
+
+var deEersteBoekButton = document.querySelector("article:nth-of-type(1) section:nth-of-type(9) li:nth-of-type(1) button")
+deEersteBoekButton.addEventListener("click", eersteBoek);
+
+function eersteBoek() {
+    var boekEen = document.querySelector("article:nth-of-type(2)")
+    boekEen.classList.add("openEersteBoek");
 }
